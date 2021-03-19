0cb71b6e3625df0cf6c9c501e28ea370205f82db
Ian Kenley <ikenley6@gmail.com>
2021-03-19 19:04:06 -0400

    buildspec docker IMAGE_TAG

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       Open the pod bay doors, HAL.
-      Real content coming soon. In the meantime feast your eyes on these links.
+      Real content coming soon. In the meantime feast your eyes on these links.{" "}
       <a href="https://github.com/ikenley">Github</a>
     </div>
   );
