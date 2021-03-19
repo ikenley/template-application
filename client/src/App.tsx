5f87abb6b32b0aa15f2e8451134939bf2d23e651
Ian Kenley <ikenley6@gmail.com>
2021-03-19 15:53:04 -0400

    added link to github

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       Open the pod bay doors, HAL.
+      Real content coming soon. In the meantime feast your eyes on these links.
+      <a href="https://github.com/ikenley">Github</a>
     </div>
   );
 }
