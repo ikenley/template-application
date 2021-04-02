c54b0d1509f1a50abb4b8c521c5d3a0602aaecbf
Ian Kenley <ikenley6@gmail.com>
2021-04-02 10:09:14 -0400

    explainer: tooltips, resource menu, faq

--- client/src/overview/FilterPanel.tsx
+++ client/src/overview/FilterPanel.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Col, Form, InputGroup } from "react-bootstrap";
+import {
+  Col,
+  Form,
+  InputGroup,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
 
 const FilterPanel = () => {
   return (
@@ -16,6 +22,25 @@ const FilterPanel = () => {
               <InputGroup.Prepend>
                 <InputGroup.Text id="basic-addon1">
                   Market Share Model
+                  <OverlayTrigger
+                    placement="bottom"
+                    overlay={
+                      <Tooltip id="market-share-model-button">
+                        Proin dapibus nisl id massa varius, mattis blandit metus
+                        maximus. Duis eu massa vitae ligula ultrices laoreet.
+                        Nulla fermentum elit eget lobortis mollis. Ut metus
+                        velit, vulputate quis tincidunt sed, condimentum
+                        ultricies tellus. Etiam interdum porttitor libero
+                        volutpat mattis. Nullam consequat mi non nisl
+                        pellentesque, non suscipit lacus aliquet. Mauris ac quam
+                        lacus.
+                      </Tooltip>
+                    }
+                  >
+                    <span className="ml-1">
+                      <i className="fas fa-info-circle"></i>
+                    </span>
+                  </OverlayTrigger>
                 </InputGroup.Text>
               </InputGroup.Prepend>
               <Form.Control as="select">
--- client/src/shared/Navbar.tsx
+++ client/src/shared/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 const NavbarMain = () => {
@@ -23,6 +23,38 @@ const NavbarMain = () => {
           <NavLink className="nav-link" activeClassName="active" to="/market">
             COVID-19
           </NavLink>
+        </Nav>
+        <Nav className="ml-auto">
+          <NavDropdown
+            title={<span>Resources</span>}
+            id="navbar-context-menu"
+            className="navbar-context-menu"
+            alignRight
+          >
+            <NavDropdown.Item href="#action/launch_tour_todo">
+              Walkthrough
+            </NavDropdown.Item>
+            <NavDropdown.Item target="_blank" href="/faq.pdf">
+              FAQ
+            </NavDropdown.Item>
+            <NavDropdown.Item href="mailto:todo@todo.com">
+              Contact Us
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item target="_blank" href="https://www.google.com">
+              Related Resource X
+            </NavDropdown.Item>
+            <NavDropdown.Item target="_blank" href="https://www.google.com">
+              Related Resource Y
+            </NavDropdown.Item>
+            <NavDropdown.Item target="_blank" href="https://www.google.com">
+              Related Resource Z
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/logout_todo">
+              Sign Out
+            </NavDropdown.Item>
+          </NavDropdown>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
